2b1c211396de05773bf2590f87c7ce6ef6972459
OmarMohamed11 <omar.furp62@gmail.com>
2020-08-12 22:23:54 +0200

    return data in body

--- functions/serve.js
+++ functions/serve.js
@@ -1,4 +1,8 @@
 exports.handler =  async  event =>{
    const data =  require(`../data/${event.queryStringParameters.fileName}`);
     console.log(data);
+    return {
+        statusCode: 200,
+        body: data
+      }
 }
