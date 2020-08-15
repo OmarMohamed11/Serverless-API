4f6a1334c159cc030d11080b5434ce934c64cc73
OmarMohamed11 <omar.furp62@gmail.com>
2020-08-16 01:51:55 +0200

    throw errors

--- functions/serve.js
+++ functions/serve.js
@@ -1,11 +1,16 @@
 exports.handler =  async  event =>{
-    let {path} = event;
-    let file = path.split("/")[4];
-   const data =  require(`../data/${file}`);
-   console.log(event.path)
-    console.log(data);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({data})
-      }
+    try {
+        let {path} = event;
+        let file = path.split("/")[4];
+       const data =  require(`../data/${file}`);
+       console.log(event.path)
+        console.log(data);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({data})
+          }
+    } catch (error) {
+        throw(error);
+    }
+
 }
