91d4f6308157ea70d2e1474d90fd8bf97910c41d
OmarMohamed11 <omar.furp62@gmail.com>
2020-08-16 02:13:47 +0200

    handling different errors

--- functions/serve.js
+++ functions/serve.js
@@ -2,16 +2,19 @@ exports.handler =  async  event =>{
     try {
         let {path} = event;
         let file = path.split("/")[4];
-       const data =  require(`../data/${file}`);
-       console.log(event.path)
+        const data =  require(`../data/${file}`);
+        console.log(event.path)
         console.log(data);
         return {
             statusCode: 200,
             body: JSON.stringify({data})
-          }
+        }
     } catch (error) {
-        if (error.errorMessage == "Cannot find module './undefined'") {
-            error.errorMessage = "please specify file to fetch"
+        if (error.message  == "Cannot find module './undefined'" || error.message  == "Cannot find module './'") {
+            error.message  = "please specify file to fetch"
+        }else {
+            let unfoundFile = error.message .split("/")[1].slice(0,error.message .split("/")[1].length-1);
+            error.message  = `${unfoundFile} is not a file`
         }
         throw(error);
     }
